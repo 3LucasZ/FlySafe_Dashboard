7f9a96bb9c9fc8de59ee8a942d5685664a64a7c2
3LucasZ <lucas.zheng@warriorlife.net>
2023-05-31 17:47:33 -0700

    Added reboot + offset (beta)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+//div by id
 const canvasDiv = document.getElementById("canvasDiv");
-var distDiv = document.getElementById("distDiv");
-var statusDiv = document.getElementById("statusDiv");
-var volDiv = document.getElementById("volDiv");
+const distDiv = document.getElementById("distDiv");
+const statusDiv = document.getElementById("statusDiv");
+const volDiv = document.getElementById("volDiv");
+const offsetDiv = document.getElementById("offsetDiv");
+const offsetInputDiv = document.getElementById("offsetInputDiv");
+
+//running variables
 var dists = [0];
 var cnts = [0];
 var curVol;
+var curOffset;
+
+//chart
 var chart = new Chart(canvasDiv, {
   type: "line",
   data: {
@@ -61,15 +69,18 @@ function reGraph() {
     },
   });
 }
-//set up
-//set this to the service uuid of your device
+
+//set up BLE
 const serviceUuid = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
 const CHAR_DIST_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 const CHAR_VOL_UUID = "66d04e60-89ba-4ab2-a0b3-2257bc8d43f7";
-const CHAR_SPEAKER_UUID = "90788378-fca7-48da-9b30-a0bbfeacdb7b";
+const CHAR_OFFSET_UUID = "8c3b12cb-3445-4961-b9af-c49521dc9d7a";
+const CHAR_REBOOT_UUID = "0d006e04-39d4-4d90-ae33-e278cbc6dc66";
 let distChar;
 let volChar;
 let speakerChar;
+let offsetChar;
+let rebootChar;
 let ble = new p5ble();
 
 function connectBLE() {
@@ -81,7 +92,7 @@ function disconnectBLE() {
   statusDiv.innerHTML = "Connected: " + ble.isConnected();
 }
 
-// A function that will be called once characteristics are received
+//called once characteristics are received
 function gotCharacteristics(error, characteristics) {
   statusDiv.innerHTML = "Connected: " + ble.isConnected();
   if (ble.isConnected()) {
@@ -92,12 +103,37 @@ function gotCharacteristics(error, characteristics) {
     }
     distChar = characteristics[uuids.indexOf(CHAR_DIST_UUID)];
     volChar = characteristics[uuids.indexOf(CHAR_VOL_UUID)];
-    speakerChar = characteristics[uuids.indexOf(CHAR_SPEAKER_UUID)];
-
+    offsetChar = characteristics[uuids.indexOf(CHAR_OFFSET_UUID)];
+    rebootChar = characteristics[uuids.indexOf(CHAR_REBOOT_UUID)];
     ble.read(volChar, gotVol);
   }
 }
 
+//init r/w
+function gotVol(error, value) {
+  statusDiv.innerHTML = "Connected: " + ble.isConnected();
+  if (!ble.isConnected()) return;
+  if (error) console.log("error: ", error);
+  console.log("vol: ", value);
+  volDiv.innerHTML = value;
+  curVol = value;
+  setTimeout(() => {
+    ble.read(offsetChar, gotOffset);
+  }, 50);
+}
+function gotOffset(error, value) {
+  statusDiv.innerHTML = "Connected: " + ble.isConnected();
+  if (!ble.isConnected()) return;
+  if (error) console.log("error: ", error);
+  console.log("offset: ", value);
+  offsetDiv.innerHTML = value;
+  curOffset = value;
+  setTimeout(() => {
+    ble.read(distChar, gotDist);
+  }, 50);
+}
+
+//looped r/w
 function gotDist(error, value) {
   statusDiv.innerHTML = "Connected: " + ble.isConnected();
   if (!ble.isConnected()) return;
@@ -111,27 +147,26 @@ function gotDist(error, value) {
   window.speechSynthesis.speak(msg);
   setTimeout(() => {
     writeVol();
-  }, 1000);
+  }, 500);
 }
-function gotVol(error, value) {
-  statusDiv.innerHTML = "Connected: " + ble.isConnected();
-  if (!ble.isConnected()) return;
-  if (error) console.log("error: ", error);
-  console.log("vol: ", value);
-  volDiv.innerHTML = value;
-  curVol = value;
-  setTimeout(() => {
-    ble.read(distChar, gotDist);
-  }, 1000);
-}
+
 function writeVol() {
   statusDiv.innerHTML = "Connected: " + ble.isConnected();
   if (!ble.isConnected()) return;
   ble.write(volChar, curVol);
   console.log("Write vol: ", curVol);
   setTimeout(() => {
+    writeOffset();
+  }, 500);
+}
+function writeOffset() {
+  statusDiv.innerHTML = "Connected: " + ble.isConnected();
+  if (!ble.isConnected()) return;
+  ble.write(offsetChar, curOffset);
+  console.log("Write offset: ", curOffset);
+  setTimeout(() => {
     ble.read(distChar, gotDist);
-  }, 1000);
+  }, 500);
 }
 
 //state changers
@@ -148,6 +183,12 @@ function volDown() {
   curVol -= 5;
   curVol = Math.max(curVol, 0);
   volDiv.innerHTML = curVol;
+}
+function setOffset() {
+  statusDiv.innerHTML = "Connected: " + ble.isConnected();
+  if (!ble.isConnected()) return;
+  curOffset = offsetInputDiv.value;
+  offsetDiv.innerHTML = curOffset;
 }
 
 if ("speechSynthesis" in window) {
