790dd33310d7096f1a9b2e1442f23e1b859a4f83
3LucasZ <lucas.zheng@warriorlife.net>
2023-11-18 00:06:24 -0800

    gg

--- src/js/download.js
+++ src/js/download.js
@@ -3,7 +3,7 @@ function downloadFlightData() {
   download(content);
 }
 function download(content) {
-  var file = new File(["\ufeff" + content], "myFile.txt", {
+  var file = new File(["\ufeff" + content], "hello.txt", {
     type: "text/plain:charset=UTF-8",
   });
   url = window.URL.createObjectURL(file);
@@ -12,12 +12,16 @@ function download(content) {
 
   //a.style = "display: none";
   secretDiv.href = url;
-  logsDiv.innerHTML = url + "HEY THERE" + content;
+  logsDiv.innerHTML = url + " | " + content + " | " + file.webkitRelativePath;
   //secretDiv.download = file.name;
   //a.click();
   //window.URL.revokeObjectURL(url);
 }
 function abc() {
-  var newWindow = window.open();
-  newWindow.document.write("ohai");
+  // var newWindow = window.open();
+  // newWindow.document.write("ohai");
+  // var html = "<html><head></head><body>ohai</body></html>";
+  // var uri = "data:text/html," + encodeURIComponent(html);
+  // var newWindow = window.open(uri);
+  //window.open("OHAI");
 }
